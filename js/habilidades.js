6b5da812577dde13ecb89a0643c59cd2db67e57b
diogo <diogo.marcondes2004@gmail.com>
2023-02-09 21:01:27 -0300

    first commit

---
+++ js/habilidades.js
@@ -0,0 +1,57 @@
+const htmlDescription = document.querySelector('.html');
+const littleText = document.querySelector('.none')
+const css = document.querySelector('.css')
+const js = document.querySelector('.js')
+const sass = document.querySelector('.sass')
+const git = document.querySelector('.git')
+const github = document.querySelector('.github')
+function htmlText(){
+    if(littleText.classList != 'w'){
+        littleText.classList.add('textDescription')
+        littleText.style.display = 'block'
+        littleText.innerHTML = 'Html é uma linguagem de marcação'
+    }
+}
+htmlDescription.addEventListener('click', htmlText)
+
+function cssText(){
+    if(littleText.classList != 'w'){
+        littleText.classList.add('textDescription')
+        littleText.style.display = 'block'
+        littleText.innerHTML = 'Css é uma linguagem de estilização'
+    }
+}
+css.addEventListener('click', cssText)
+
+function jsText(){
+    if(littleText.classList != 'w'){
+        littleText.classList.add('textDescription')
+        littleText.style.display = 'block'
+        littleText.innerHTML = 'Javascript é uma linguagem de programação orientada a objetos'
+    }
+}
+js.addEventListener('click', jsText)
+function sassText(){
+    if(littleText.classList != 'w'){
+        littleText.classList.add('textDescription')
+        littleText.style.display = 'block'
+        littleText.innerHTML = 'Sass é como se fosse um complemento do css'
+    }
+}
+sass.addEventListener('click', sassText)
+function gitText(){
+    if(littleText.classList != 'w'){
+        littleText.classList.add('textDescription')
+        littleText.style.display = 'block'
+        littleText.innerHTML = 'Git é um controle de sistema de versões'
+    }
+}
+git.addEventListener('click', gitText)
+function githubText(){
+    if(littleText.classList != 'w'){
+        littleText.classList.add('textDescription')
+        littleText.style.display = 'block'
+        littleText.innerHTML = 'Git hub é uma plataforma de hospedagem de código fonte'
+    }
+}
+github.addEventListener('click', githubText)
---
+++ js/menu.js
@@ -0,0 +1,8 @@
+const buttonMenu = document.querySelector('.menu-button');
+
+function mobileMenu(){
+    const nav = document.querySelector('.ul-menu');
+    nav.classList.toggle('menu-off')
+}
+
+buttonMenu.addEventListener('click', mobileMenu)
