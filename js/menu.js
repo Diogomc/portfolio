972b06a24473c77bed997ca02732a52ca9a77b6e
diogo <diogo.marcondes2004@gmail.com>
2023-02-12 21:21:13 -0300

    mobile

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,12 @@
 const buttonMenu = document.querySelector('.menu-button');
-
+const drop = document.querySelector('.drop')
 function mobileMenu(){
     const nav = document.querySelector('.ul-menu');
     nav.classList.toggle('menu-off')
 }
 
 buttonMenu.addEventListener('click', mobileMenu)
+
+
+
+
